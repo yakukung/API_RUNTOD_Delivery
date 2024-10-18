dff0c310b314fbbef692a3576b5c9524ae7e21a0
yakukung <yakukung2003@gmail.com>
2024-10-19 04:47:15 +0700

    ออ

--- database/database.js
+++ database/database.js
@@ -3,7 +3,7 @@ const db = new sqlite3.Database("./database/delivery.db", (err) => {
     if (err) {
         console.error(err.message);
     } else {
-        console.log("Connected to the LOTTO database.");
+        console.log("Connected database.");
     }
 });
 
