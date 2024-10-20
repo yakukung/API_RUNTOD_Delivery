d9717fea1e1dca46fb75ae3473769a290852a4ec
yakukung <yakukung2003@gmail.com>
2024-10-20 19:49:46 +0700

    อัพเดต

--- routes/users/users.js
+++ routes/users/users.js
@@ -4,7 +4,7 @@ const db = require("../../database/database.js");
 const helpers = require("../../helpers/helpers.js");
 const handleResponse = helpers.handleResponse;
 
-
+//แสดงข้อมูลทั้งหมดของผู้ใช้ id นั้น
 router.get("/:id", (req, res) => {
     const id = req.params.id;
     db.get("SELECT * FROM users WHERE uid = ?", [id], (err, row) => {
@@ -17,5 +17,26 @@ router.get("/:id", (req, res) => {
         return handleResponse(res, null, row);
     });
 });
+
+//อัพเดตที่อยู่รับของ
+router.put("/update/address", (req, res) => {
+    const { uid, _address } = req.body;
+    db.run(
+        "UPDATE users SET receiving_address = ? WHERE uid = ?",
+        [ _address, uid],
+        function (err) {
+          handleResponse(
+            res,
+            err,
+            { message: "updated successfully" },
+            404,
+            "not found",
+            this.changes
+          );
+        }
+      );
+});
+
+
 
 module.exports = router;
