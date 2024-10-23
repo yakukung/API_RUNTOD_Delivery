063f92d0fc04b13960d709a9e7c437c39e313daa
yakukung <yakukung2003@gmail.com>
2024-10-23 20:08:50 +0700

    เพิ่ม user/orders

--- routes/users/orders.js
+++ routes/users/orders.js
@@ -4,7 +4,7 @@ const db = require("../../database/database.js");
 const helpers = require("../../helpers/helpers.js");
 const handleResponse = helpers.handleResponse;
 
-
+// ค่าเริ่มต้น http://192.168.1.4:3000 >อย่าลืมเปลี่ยนที่อยุ่นี่< /user/orders
 
 
 module.exports = router;
